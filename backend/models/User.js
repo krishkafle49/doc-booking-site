9ae98e35199db528a446c8a64343f4ad3e9d48b3
krishkafle49 <kaflekrish49@gmail.com>
2025-04-21 11:24:11 +0545

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['doctor', 'patient', 'admin'],
+    default: 'patient',
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
 
